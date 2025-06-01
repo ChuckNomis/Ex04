d448d36aa169f8735402cdc91c31db96f7c815c7
NadavSi <52112772+ChuckNomis@users.noreply.github.com>
2025-06-01 12:07:05 +0300

    nadav push

---
+++ Ex04/Ex04.Menus.Events/MainMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Ex04.Menus.Events
+{
+    internal class MainMenu
+    {
+        private readonly MenuItem r_RootMenu;
+
+        public MainMenu(string i_Title)
+        {
+            r_RootMenu = new MenuItem(i_Title);
+        }
+
+        public void AddMenuItem(MenuItem i_Item)
+        {
+            r_RootMenu.AddSubItem(i_Item);
+        }
+
+        public void Show()
+        {
+            navigateMenu(r_RootMenu);
+        }
+
+        private void navigateMenu(MenuItem i_CurrentMenu)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"** {i_CurrentMenu.m_title} **");
+                Console.WriteLine(new string('-', i_CurrentMenu.m_title.Length + 6));
+
+                for (int i = 0; i < i_CurrentMenu.SubItems.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {i_CurrentMenu.SubItems[i].m_title}");
+                }
+                string backOrExit = i_CurrentMenu.Parent == null ? "Exit" : "Back";
+                Console.WriteLine($"0. {backOrExit}");
+                Console.Write($"Please enter your choice (1-{i_CurrentMenu.SubItems.Count} or 0 to {backOrExit}): ");
+
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int choice) ||
+                    choice < 0 || choice > i_CurrentMenu.SubItems.Count)
+                {
+                    Console.WriteLine("Invalid input, press any key to continue...");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if (choice == 0)
+                {
+                    if (i_CurrentMenu.Parent == null)
+                        break;
+                    else
+                        return;
+                }
+
+                MenuItem selectedItem = i_CurrentMenu.SubItems[choice - 1];
+
+                if (selectedItem.HasSubItems)
+                {
+                    navigateMenu(selectedItem);
+                }
+                else
+                {
+                    Console.Clear();
+                    selectedItem.Activate();
+                    Console.WriteLine("Press any key to return...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Ex04/Ex04.Menus.Events/MenuItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Events
+{
+    internal class MenuItem
+    {
+        public string m_title { get; }
+        private readonly List<MenuItem> r_subItems;
+        private readonly Action r_action;
+
+        public IReadOnlyList<MenuItem> SubItems => r_subItems.AsReadOnly();
+        public bool HasSubItems => r_subItems.Count > 0;
+        public MenuItem Parent { get; private set; }
+
+        public MenuItem(string i_Title)
+        {
+            m_title = i_Title;
+            r_subItems = new List<MenuItem>();
+        }
+
+        public MenuItem(string i_Title, Action i_Action)
+            : this(i_Title)
+        {
+            r_action = i_Action;
+        }
+
+        public void AddSubItem(MenuItem i_SubItem)
+        {
+            i_SubItem.Parent = this;
+            r_subItems.Add(i_SubItem);
+        }
+
+        internal void Activate()
+        {
+            r_action?.Invoke();
+        }
+    }
+}
