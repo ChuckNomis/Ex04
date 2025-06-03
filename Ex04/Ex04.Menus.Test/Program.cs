7dcb9fb6bdd11b7f1061520c5409f19da3e9fce9
NadavSi <52112772+ChuckNomis@users.noreply.github.com>
2025-06-03 07:01:57 +0300

    s

--- Ex04/Ex04.Menus.Test/Program.cs
+++ Ex04/Ex04.Menus.Test/Program.cs
@@ -31,3 +31,69 @@ namespace Ex04.Menus.Test
         }
     }
 }
+/* 
+ ﻿using System;
+using Ex04.Menus.Events;
+
+namespace Ex04.Menus.Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MainMenu delegatesMenu = buildMenuWithDelegates();
+            delegatesMenu.Show();
+        }
+
+        private static MainMenu buildMenuWithDelegates()
+        {
+            MainMenu mainMenu = new MainMenu("Delegates Main Menu");
+
+            MenuItem lettersAndVersion = new MenuItem("Letters and Version");
+            lettersAndVersion.AddSubItem(new MenuItem("Show Version", showVersion));
+            lettersAndVersion.AddSubItem(new MenuItem("Count Lowercase Letters", countLowerCase));
+
+            MenuItem showDateTime = new MenuItem("Show Current Date/Time");
+            showDateTime.AddSubItem(new MenuItem("Show Current Time", showTime));
+            showDateTime.AddSubItem(new MenuItem("Show Current Date", showDate));
+
+            mainMenu.AddMenuItem(lettersAndVersion);
+            mainMenu.AddMenuItem(showDateTime);
+
+            return mainMenu;
+        }
+
+        private static void showVersion()
+        {
+            Console.WriteLine("App Version: 25.2.4.4480");
+        }
+
+        private static void countLowerCase()
+        {
+            Console.Write("Enter a sentence: ");
+            string input = Console.ReadLine();
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsLower(c))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"There are {count} lowercase letters in your text.");
+        }
+
+        private static void showTime()
+        {
+            Console.WriteLine($"Current Time is {DateTime.Now:HH:mm}");
+        }
+
+        private static void showDate()
+        {
+            Console.WriteLine($"Current Date is {DateTime.Now:yyyy-MM-dd}");
+        }
+    }
+}
+ */
